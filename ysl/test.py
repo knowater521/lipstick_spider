e727ac29f44d21a46dfb8aa345344d0e72329d29
polaris <polaris1478@gmail.com>
2018-12-06 22:23:09 +0800

    ysl手工爬

--- ysl/test.py
+++ ysl/test.py
@@ -1,9 +1,35 @@
 # -*- coding: utf-8 -*-
+import time
 from pyquery import PyQuery as pq
 
 if __name__ == "__main__":
-    d = pq(url='https://www.yslbeautycn.com/plp/list.json?ni=15&s=category_order_14_desc%2Cdefault_sort_asc&pn=1&pageSize=20')
-    a = d(".goods-introudce a")
-    print(len(a))
-    for aa in a:
-        print(pq(aa).attr('href'))
+    host = "https://www.yslbeautycn.com"
+    d = pq(url=host + '/makeup-lipstick')
+    # 分类
+    catalog = d(".list-inline li:last").text()
+
+    # 产品信息
+    productInfo = {}
+    for p in d(".goods-introudce a"):
+        this = pq(p)
+        productInfo[this.text()] = this.attr("href")
+
+    res = []
+    for product, url in productInfo.items():
+        time.sleep(0.5)
+        dd = pq(url=host + url)
+        details = {}
+        for item in dd('.product-color-select').find('.sub-menu.tinyscrollbar li'):
+            that = pq(item)
+            name = pq(item).find('span:last').text().split(" ")
+            color_number, name = name[0], ''.join(name[1:])
+            res.append({
+                'id': that.attr('code'),
+                'catalog': catalog,
+                'url': url,
+                'product': product,
+                'color_number': color_number,
+                'name': name,
+                'img': pq(item).find('img').attr('src'),
+            })
+    print(res)
